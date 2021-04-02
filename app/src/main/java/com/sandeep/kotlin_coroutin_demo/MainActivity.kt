d91af3221f901b1bb207d8ae29bd6ba9138f2100
sks1992 <sandeepSK1581992@gmail.com>
2021-04-02 11:15:00 +0530

    coroutine-suspend-function

--- app/src/main/java/com/sandeep/kotlin_coroutin_demo/MainActivity.kt
+++ app/src/main/java/com/sandeep/kotlin_coroutin_demo/MainActivity.kt
@@ -17,11 +17,28 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch{
 
-            delay(3000L)
-            Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+           // delay(3000L)
+            //Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+
+           val networkCallAnswer= networkCall()
+            val networkCallAnswer2= networkCall2()
+
+            Log.d(TAG,networkCallAnswer)
+            Log.d(TAG,networkCallAnswer2)
         }
 
-        Log.d(TAG,"Hello from thread ${Thread.currentThread().name}")
+       // Log.d(TAG,"Hello from thread ${Thread.currentThread().name}")
 
+    }
+
+    suspend fun networkCall() :String{
+
+        delay(3000L)
+        return "This is the answer 1"
+    }
+    suspend fun networkCall2() :String{
+
+        delay(3000L)
+       return  "This is the answer 2"
     }
 }
